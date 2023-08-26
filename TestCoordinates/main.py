fb92176464ece3893584f597a194af94754fd5ea
smashok <65979450+smashok@users.noreply.github.com>
2023-08-26 23:36:00 +0300

    Commit 2
    Added code

--- TestCoordinates/main.py
+++ TestCoordinates/main.py
@@ -1,14 +1,43 @@
+import math
 from geopy.distance import geodesic
+from geopy import Point
 
-current_coord = (50.603694,30.650625)
+# Координаты контрольной точки
+Lat1 = 50.603694
+Lon1 = 30.650625
+point_x_center = 558
+point_y_center = 328
 
-distance_west_meters = 93.53
-bearing_west = 270  #западное направление
+# Координаты центра изображения
+center_x = 320
+center_y = 256
 
-distance_south_meters = 13.36
-bearing_south = 180  #южное направление
+# Азимут и количество метров в пикселе
+azimut = 335
+pixel_coefficient = 0.38
 
-new_coord_west = geodesic(kilometers=distance_west_meters / 1000).destination(current_coord, bearing_west)
-new_coord_final = geodesic(kilometers=distance_south_meters / 1000).destination(new_coord_west, bearing_south)
+# Ищем расстояние между точками
+big_leg = abs(point_x_center - center_x) * pixel_coefficient
+small_leg = abs(point_y_center - center_y) * pixel_coefficient
+hypotenuse = math.sqrt(big_leg ** 2 + small_leg ** 2)
 
-print("Новые координаты:", new_coord_final.latitude, new_coord_final.longitude)
+# Ищем угол между направлением движения дрона и отрезком
+cosine_value = small_leg / hypotenuse
+angle_rad = math.acos(cosine_value)
+angle_deg = math.degrees(angle_rad)
+
+# Ищем азимут направления от контрольной точки до искомой
+angle_between_dots = azimut - angle_deg
+
+# Создание объекта Point для контрольной точки
+control_point = Point(Lat1, Lon1)
+
+# Вычисление координат
+destination = geodesic(kilometers=hypotenuse / 1000).destination(control_point, angle_between_dots)
+Lat2, Lon2 = destination.latitude, destination.longitude
+
+# Ссылка на гугл карты и вывод
+google_maps_url = f"https://www.google.com/maps?q={Lat2},{Lon2}&t=k"
+
+print(google_maps_url)
+
